0602a6a81e02b3b2500f86f4fb34f05f779bd3be
Bertrand BRUN <bertrand0brun@gmail.com>
2011-01-20 15:46:43 +0100

    Ajout de la préférence « Serveur ». Ajout d'une information sur l'identité du joueur dans la page de garde. Changement du nom du bouton « Jeux » -> « Jouer »

--- code/PtiClic/src/org/pticlic/Main.java
+++ code/PtiClic/src/org/pticlic/Main.java
@@ -2,19 +2,27 @@ package org.pticlic;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Main extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */
+  
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         ((Button)findViewById(R.id.prefs)).setOnClickListener(this);
         
+        // On récupère le nom du joueur des préférences.
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String loginPref = sp.getString("login", "joueur");
+        ((TextView)findViewById(R.id.login)).setText("Login : " + loginPref);
     }
 
 	@Override
