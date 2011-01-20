0d843867d7577db3413b096dd396080c69208b2d
Bertrand BRUN <bertrand0brun@gmail.com>
2011-01-20 13:15:34 +0100

    Lorsque l'on clique sur « Préférences », le panneau de préférence s'ouvre. Désactivation du bouton « Jeux ». Suppression de la barre de titre.

--- code/PtiClic/src/org/pticlic/Main.java
+++ code/PtiClic/src/org/pticlic/Main.java
@@ -1,13 +1,26 @@
 package org.pticlic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Main extends Activity {
+public class Main extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ((Button)findViewById(R.id.prefs)).setOnClickListener(this);
+        
     }
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId()==R.id.prefs) {
+			startActivity(new Intent(this, Preference.class));
+		}
+	}
 }
