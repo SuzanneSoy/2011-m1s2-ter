984d28890cf2cba1cc05def7b7744745b3296a62
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-01-27 19:07:40 +0100

    Début de l'interface du Jeu.

--- code/PtiClic/src/org/pticlic/Main.java
+++ code/PtiClic/src/org/pticlic/Main.java
@@ -11,25 +11,31 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Main extends Activity implements OnClickListener {
-  
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        ((Button)findViewById(R.id.prefs)).setOnClickListener(this);
-        
-        // On récupère le nom du joueur des préférences.
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        String loginPref = sp.getString("login", "joueur");
-        // On l'ajoute dans le TextView prévu à cet effet
-        ((TextView)findViewById(R.id.login)).setText("Login : " + loginPref);
-    }
 
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// Écoute des clics sur les différents boutons
+		((Button)findViewById(R.id.prefs)).setOnClickListener(this);
+		((Button)findViewById(R.id.play)).setOnClickListener(this);
+
+		// On récupère le nom du joueur des préférences.
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+		String loginPref = sp.getString("login", "joueur");
+		// On l'ajoute dans le TextView prévu à cet effet
+		((TextView)findViewById(R.id.login)).setText("Login : " + loginPref);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
 	@Override
 	public void onClick(View v) {
-		if (v.getId()==R.id.prefs) {
-			startActivity(new Intent(this, Preference.class));
+		switch (v.getId()) {
+		case (R.id.prefs) : startActivity(new Intent(this, Preference.class)); break;
+		case (R.id.play) : startActivity(new Intent(this, Game.class)); break;
 		}
 	}
 }
