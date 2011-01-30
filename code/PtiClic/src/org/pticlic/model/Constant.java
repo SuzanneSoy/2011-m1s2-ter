f0b8b197b3398bfe0e14493ad080f1e9f42f7a34
Bertrand BRUN <bertrand0brun@gmail.com>
2011-01-30 16:49:49 +0100

    Ajout de la classe Score et de la connection entre BaseGame et Score

--- code/PtiClic/src/org/pticlic/model/Constant.java
+++ code/PtiClic/src/org/pticlic/model/Constant.java
@@ -2,4 +2,7 @@ package org.pticlic.model;
 
 public class Constant {
 	public static final String SERVER_URL = "SERVER_URL";
+	
+	// Constant pour les intents
+	public static final String SCORE_INTENT = "SCORE_INTENT";
 }
