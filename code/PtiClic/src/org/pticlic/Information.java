52b91e4425eed130b3df6a441e7cc8b416391c43
Bertrand BRUN <bertrand0brun@gmail.com>
2011-01-31 23:56:17 +0100

    Modification dans la classe Information et dans l'affichage de celle-ci

--- code/PtiClic/src/org/pticlic/Information.java
+++ code/PtiClic/src/org/pticlic/Information.java
@@ -1,7 +1,47 @@
 package org.pticlic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class Information extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+		
+		((TextView)findViewById(R.id.infoVersion)).setText("version : " + getString(R.string.version));
+		InputStream in = getResources().openRawResource(R.raw.info);
+		WebView webview = (WebView)findViewById(R.id.textContent);
+		webview.setBackgroundColor(Color.BLACK);
+		webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+		
+		if (in != null) {
+			
+			InputStreamReader tmp = new InputStreamReader(in);
+			BufferedReader reader = new BufferedReader(tmp);
+			String html;
+			StringBuffer buf = new StringBuffer();
+			
+			try {
+				while ((html = reader.readLine()) != null) {
+					buf.append(html + "\n");
+				}
+			
+			in.close();
+			webview.loadData(buf.toString(), "text/html", "UTF-8");
+			
+			} catch (IOException e) {
+				//TODO : Ajouter un boite de dialog indiquant qu'une erreur est arrivee.
+			}
+		}
+	}
 }
--- code/PtiClic/src/org/pticlic/Main.java
+++ code/PtiClic/src/org/pticlic/Main.java
@@ -10,6 +10,7 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main extends Activity implements OnClickListener {
@@ -22,6 +23,7 @@ public class Main extends Activity implements OnClickListener {
 		// Écoute des clics sur les différents boutons
 		((Button)findViewById(R.id.prefs)).setOnClickListener(this);
 		((Button)findViewById(R.id.play)).setOnClickListener(this);
+		((ImageView)findViewById(R.id.infoButton)).setOnClickListener(this);
 
 		// On récupère le nom du joueur des préférences.
 		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
@@ -38,17 +40,7 @@ public class Main extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case (R.id.prefs) : startActivity(new Intent(this, Preference.class)); break;
 		case (R.id.play) : startActivity(new Intent(this, BaseGame.class)); break;
-		}
-		if (v.getId()==R.id.voirscore){
-	        // TODO: à supprimer
-	        /*
-	        Intent intent = new Intent(this, Score.class);
-	        intent.putExtra("corrects", 10);
-	        intent.putExtra("mauvais", 2);
-	        intent.putExtra("manquants", 1.5);
-	        intent.putExtra("total", 10); // WHERE??
-	        startActivity(intent);
-	        */
+		case (R.id.infoButton) : startActivity(new Intent(this, Information.class)); break;
 		}
 	}
 	
