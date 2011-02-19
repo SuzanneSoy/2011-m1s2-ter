0238a2571ded409f4a41c66156def0ced795d883
John Charron <rm_rf_windows@yahoo.fr>
2011-02-19 23:00:50 +0100

    Tout premier jet de la page d'accueil (jc).

--- code/serveur/php/index.php
+++ code/serveur/php/index.php
@@ -16,10 +16,32 @@ session_start();
 			<?php include("ressources/menu.html"); ?>	
 		</div>
 		<div class="content">
-			<h2>Le projet</h2>
-			<p> Le projet...</p>
-			<h2>La beta</h2>
-			<p> La version beta de PtiClic...</h2>
+			<h2>Jeu du PtiClic - Téléchargement gratuit</h2>
+			<p>Vous aimer les jeux de mots&nbsp;? Vous avez un smartphone Android&nbsp;?
+                            PtiClic est pour vous&nbsp;!</p>
+                        <p>Soyez parmi les tous premiers à télécharger cette application gratuitement
+                            en devenant Alpha-testeur. L'inscription est simple. Il suffit de fournir une adresse mail, de créer un
+                            identifiant et vous pourrez commencer à jouer !</p>
+                        <p>Il ne s'agit pas d'une version d'essai et l'application ne contient aucun
+                            spyware.</p>
+
+                            <h2>Le principe du jeu</h2>
+                        <p>Un mot central apparaît ainsi que quatre associations
+                            telles que "synonyme", "antonyme", "est une sorte de", "corbeille". L'idée est
+                            de lier de nouveaux mots au mot central à l'aide des associations.
+                            Plus votre réponse est juste, plus vous gagner des points.
+                            Attention, vous pouvez aussi perdre des points&nbsp;!</p>
+
+			<h2>Le développement de l'application</h2>
+			<p>La version beta du PtiClic sous Android est en cours de développement.
+                        Le projet s'inscrit dans le cadre d'un TER de Master en informatique
+                        à l'Université Montpellier II sous la direction de Mathieu LAFOURCADE. L'équipe
+                        de conception et de développement est composée de quatre étudiants&nbsp;: Bertrand BRUN,
+                        Yoann BONAVERO, John CHARRON et Georges DUPERON.</p>
+                        <h2>Votre rôle en tant qu'Alpha-testeur</h2>
+                        <p>L'application étant en phase de développement et offerte gratuitement,
+                            nous serions reconnaissant si vous pourriez nous donner votre avis, vos suggestions,
+                            vos idées. </p>
 		</div>
 		<div class="footer">
 			<?php include("ressources/footer.html"); ?>
