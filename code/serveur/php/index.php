e8df71f579c8870efb2d531edca5a242c88ac031
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-02-20 20:09:17 +0100

    Corrections discutées pendant la réunion sur Jabber.

--- code/serveur/php/index.php
+++ code/serveur/php/index.php
@@ -16,32 +16,38 @@ session_start();
 			<?php include("ressources/menu.html"); ?>	
 		</div>
 		<div class="content">
-			<h2>Jeu du PtiClic - Téléchargement gratuit</h2>
-			<p>Vous aimer les jeux de mots&nbsp;? Vous avez un smartphone Android&nbsp;?
+			<h2>Jeu PtiClic - Téléchargement gratuit</h2>
+			<p>Vous aimez les jeux de mots&nbsp;? Vous avez un smartphone sous Android&nbsp;?
                             PtiClic est pour vous&nbsp;!</p>
                         <p>Soyez parmi les tous premiers à télécharger cette application gratuitement
-                            en devenant Alpha-testeur. L'inscription est simple. Il suffit de fournir une adresse mail, de créer un
+                            en devenant Alpha-testeur. L'inscription est simple&nbsp;: Il suffit de fournir une adresse mail, de créer un
                             identifiant et vous pourrez commencer à jouer !</p>
-                        <p>Il ne s'agit pas d'une version d'essai et l'application ne contient aucun
-                            spyware.</p>
+                        <!-- <p>Il ne s'agit pas d'une version d'essai et l'application ne contient aucun
+                            spyware.</p> -->
 
                             <h2>Le principe du jeu</h2>
                         <p>Un mot central apparaît ainsi que quatre associations
                             telles que "synonyme", "antonyme", "est une sorte de", "corbeille". L'idée est
                             de lier de nouveaux mots au mot central à l'aide des associations.
-                            Plus votre réponse est juste, plus vous gagner des points.
+                            Plus votre réponse est juste, plus vous gagnez de points.
                             Attention, vous pouvez aussi perdre des points&nbsp;!</p>
 
 			<h2>Le développement de l'application</h2>
-			<p>La version beta du PtiClic sous Android est en cours de développement.
+			<p>La version beta du jeu PtiClic sous Android est en cours de développement.
                         Le projet s'inscrit dans le cadre d'un TER de Master en informatique
                         à l'Université Montpellier II sous la direction de Mathieu LAFOURCADE. L'équipe
-                        de conception et de développement est composée de quatre étudiants&nbsp;: Bertrand BRUN,
-                        Yoann BONAVERO, John CHARRON et Georges DUPERON.</p>
+                        de conception et de développement est composée de quatre étudiants&nbsp;:
+			<ul>
+				<li>Bertrand BRUN,</li>
+                        	<li>Yoann BONAVERO,</li>
+				<li>John CHARRON</li>
+				<li>Georges DUPERON.</li>
+			</ul>
+			</p>
                         <h2>Votre rôle en tant qu'Alpha-testeur</h2>
                         <p>L'application étant en phase de développement et offerte gratuitement,
                             nous serions reconnaissant si vous pourriez nous donner votre avis, vos suggestions,
-                            vos idées. </p>
+                            vos idées.</p>
 		</div>
 		<div class="footer">
 			<?php include("ressources/footer.html"); ?>
