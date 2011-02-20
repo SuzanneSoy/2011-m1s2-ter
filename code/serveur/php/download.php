f7ee981786ef1c0fb3e2f639d1e3758b6ad4927d
Bertrand BRUN <bertrand0brun@gmail.com>
2011-02-20 12:15:02 +0100

    Petit modification dans le texte de la parti d'installation de l'application a partir du telephone (les fautes d'orthographe ne sont pas corriger)

--- code/serveur/php/download.php
+++ code/serveur/php/download.php
@@ -11,32 +11,32 @@ if(!isset($_SESSION['userId']))
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
-		<title>PtiClic Android - Téléchargement</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />		
-		<link rel="stylesheet" href="ressources/simple.css" />
+	<title>PtiClic Android - Téléchargement</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />		
+	<link rel="stylesheet" href="ressources/simple.css" />
 	</head>
 	<body>
-		<div class="menu">
-			<?php include("ressources/menu.html"); ?>
-		</div>
-		<div class="content">
-			<h2>Téléchargez la dernière version :</h2>
-			> <a href="ressources/pticlic.apk" id="downloadlink">Télécharger</a>
-			<h2>Installation de l'application :</h2>
+	<div class="menu">
+	<?php include("ressources/menu.html"); ?>
+</div>
+<div class="content">
+	<h2>Téléchargez la dernière version :</h2>
+	> <a href="ressources/pticlic.apk" id="downloadlink">Télécharger</a>
+	<h2>Installation de l'application :</h2>
 			<h3> A partir du téléphone </h3>
 			<ul>			
 				<li> Téléchargez l'application en cliquant sur le lien ci-dessus.</li>
-				<li> Un fois téléchargée Android vous demande si vous voulez l'installée, selectionnez oui.</li>
+	<li> Un fois téléchargée, clique sur l'application dans la barre de notification d'Android et suivez les instructions d'installation. </li>
 				<li> Patientez quelques instant. Une fois l'installation terminée démarrez l'application.</li>
 				<li> Suivez les instructions pour le premier démarrage de l'application</li>
-			</ul>
-			<h3> A partir d'un ordinateur </h3>
+	</ul>
+	<h3> A partir d'un ordinateur </h3>
 			<ul>			
 				<li> Téléchargez sur votre ordinateur l'application en cliquant sur le lien ci-dessus. </li>
-				<li> Transférez le fichier ainsi téléchargé sur votre téléphone (par bluetooth, usb...)</li>
-				<li> Depuis votre téléphone allez cherchez sur votre carte mémoire l'application ainsi transférée.</li>
+	<li> Transférez le fichier ainsi téléchargé sur votre téléphone (par bluetooth, usb...)</li>
+	<li> Depuis votre téléphone allez cherchez sur votre carte mémoire l'application ainsi transférée.</li>
 				<li> Cliquez sur l'application et installé là sur le téléphone.</li>
-				<li> Démarrez l'application et suivez les instructions pour le premier démarrage.</li>
+	<li> Démarrez l'application et suivez les instructions pour le premier démarrage.</li>
 			</ul>
 		</div>
 		<div class="footer">
