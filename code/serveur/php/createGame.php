53f0f3de5aa3bc2d7ab6c8d23b2d1c62d411cec7
Yoann Bonavero <yoann.b87@voila.fr>
2011-02-23 17:54:23 +0100

    Début de la page de création de partie.

---
+++ code/serveur/php/createGame.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<title>PtiClic Android - Création de partie</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="ressources/simple.css" />
+	</head>
+	<body>
+		<div class="menu">
+			<?php include("ressources/menu.html"); ?>	
+		</div>
+		<div class="content">
+			<form action="createGame.php" method="POST">
+			</form>
+		</div>
+		<div class="footer">
+			<?php include("ressources/footer.html"); ?>
+		</div>
+	</body>
+</html>
