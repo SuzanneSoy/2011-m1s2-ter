9c8342827513b3f29eb61438470ff588987e733a
Yoann Bonavero <yoann.b87@voila.fr>
2011-02-23 09:27:11 +0100

    Rectification d'un problème de header sur la page "login.php".

--- code/serveur/php/login.php
+++ code/serveur/php/login.php
@@ -1,6 +1,5 @@
 <?php
 session_start();    
-include("_head.php");
 
 if(isset($_POST['loginid']) && !empty($_POST['loginid']))
 	$user =  SQLite3::escapeString($_POST['loginid']);
@@ -34,6 +33,15 @@ if(isset($user) && isset($pswd))
 else if(isset($user) or isset($pswd))
 	$msg = "Veuillez remplir tous les champs";
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<title>Titre</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="ressources/simple.css" />
+	</head>
 	<body>
 		<div class="menu">
 			<?php include("ressources/menu.html"); ?>
