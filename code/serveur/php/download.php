474f4258c53941ee6f09e2959a87e7d523c1608d
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-02-24 20:04:47 +0100

    Améliorations sur le site (voir le reste du commentaire pour les détails)
    * Lisibilité (liens qui ressortent plus, footer un peu plus gros (là il fallait une loupe :) )).
    * Affichage des messages sur la page de destination pour signup.
    * Possibilité d'afficher un message sur n'importe quelle page sur laquelle on redirect ( index.php?show_msg=ok_signup_registered par ex.).
    * Regrouppement de tous les messages dans ressources/strings.inc, pour les affichages (point précédent) et pour que la vérification de l'orthographe soit plus facile (pas besoin de lire tout le code).
    * Nettoyage des paramètres à certains endroits où ça avait été oublié (mais il en reste).
    * Correction de quelques bugs (appel à la fonction mDie qui n'existe pas par ex.).
    * On ne peut faire de redirect que sur nos pages, pour éviter un exploit par redirect.
    * Signup connecte automatiquement l'utilisateur si l'inscription a fonctionné.
    * Ajout de liens entre signup et login. Si on clique sur un des lien, la page de retour reste la bonne (par exemple download, au lieu de devenir login).
    * Ré-indentation, uniformisation du style du code (plus ou moins, et plus moins que plus).
    * Factorisation du code.
    * Correction de la couleur du lien Télécharger.
    * Correction de quelques bugs d'alignement (il en reste).

--- code/serveur/php/download.php
+++ code/serveur/php/download.php
@@ -2,46 +2,39 @@
 session_start();
 
 if(!isset($_SESSION['userId']))
-	header("location:login.php?return=download.php");
+	header("location:login.php?return=download");
 
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
-	<title>PtiClic sous Android, version Alpha - Téléchargement</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />		
-	<link rel="stylesheet" href="ressources/simple.css" />
+		<title>PtiClic sous Android, version Alpha - Téléchargement</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />		
+		<link rel="stylesheet" href="ressources/simple.css" />
 	</head>
 	<body>
-		<div class="menu">
-			<?php include("ressources/menu.inc"); ?>
-		</div>
+		<?php include("ressources/menu.inc"); ?>
 		<div class="content">
+			<?php include("ressources/showmsg.inc"); ?>
 			<span class="downloadarea"><a href="ressources/pticlic.apk" id="downloadlink">Télécharger</a></span>
 			<h2>Installation de l'application</h2>
 			<h3> A partir de votre téléphone </h3>
 			<ul>
-			    <li> <a href="ressources/pticlic.apk" />Téléchargez le fichier d'installation</a></li>
-			    <li>Une fois téléchargé, cliquez sur le fichier dans la barre de notification d'Android et suivez
-			    les instructions d'installation. Vous devrez patientez quelques instant pendant l'installation.</li>
+				<li><a href="ressources/pticlic.apk">Téléchargez le fichier d'installation</a></li>
+				<li>Une fois téléchargé, cliquez sur le fichier dans la barre de notification d'Android et suivez
+					les instructions d'installation. Vous devrez patientez quelques instant pendant l'installation.</li>
 				<li>Une fois l'installation terminée, démarrez l'application</li>
 				<li> Suivez attentivement les instructions lors du premier démarrage de l'application</li>
 			</ul>
 			<h3> A partir de votre ordinateur </h3>
 			<ul>
-			    <li><a href="ressources/pticlic.apk">Téléchargez le fichier d'installation</a> </li>
-			    <li>Transférez ce fichier sur votre téléphone à l'aide de bluetooth, une clé usb ou autre</li>
-			    <li>Depuis votre téléphone, retrouvez sur votre carte mémoire l'application que vous
-			    venez de transférer</li>
-			    <li>Cliquez sur l'application afin de l'installer sur votre téléphone</li>
-			    <li>Suivez attentivement les instructions lors du premier démarrage de l'application</li>
+				<li><a href="ressources/pticlic.apk">Téléchargez le fichier d'installation</a> </li>
+				<li>Transférez ce fichier sur votre téléphone à l'aide de bluetooth, une clé usb ou autre</li>
+				<li>Depuis votre téléphone, retrouvez sur votre carte mémoire l'application que vous
+					venez de transférer</li>
+				<li>Cliquez sur l'application afin de l'installer sur votre téléphone</li>
+				<li>Suivez attentivement les instructions lors du premier démarrage de l'application</li>
 			</ul>
 		</div>
-		<div class="footer">
-			<?php include("ressources/footer.inc"); ?>
-		</div>
+		<?php include("ressources/footer.inc"); ?>
 	</body>
 </html>
--- code/serveur/php/signup.php
+++ code/serveur/php/signup.php
@@ -1,206 +1,184 @@
 <?php
 session_start();
+require_once("ressources/strings.inc");
+require_once("ressources/locations.inc");
+
 $SQL_DBNAME = (dirname(__FILE__) . "/db");
-    if (!$db = new SQlite3($SQL_DBNAME))
-        mDie(1,"Erreur lors de l'ouverture de la base de données SQLite3");
+	if (!$db = new SQlite3($SQL_DBNAME))
+		die($strings['err_signup_dbopen']);
+
+$location = getlocation();
 
 $newpage = true;
-if(!isset($msg)){
-    $msg = array();
-}
+$msg = array();
+
 if(isset($_POST['signupemail'])){
-    $newpage = false;
-    $signupemail = $_POST['signupemail'];
-    $pattern = "/^([a-zA-Z0-9])+([\.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-]+)+/";
-    
-    if(trim($signupemail) == ""){
-        $msg[] = "Veuillez renseigner le champ 'Saisir votre adresse mail'.";
-        unset($_POST['signupemail']);
-        $signupemail = "";
-    }
-    else if (!preg_match($pattern, $signupemail)){
-        $msg[] = "Adresse mail invalide. Vous pouvez utiliser des lettres, des chiffres et
-            les caractères spéciaux '-', '_' et '.'";
-	$signupemail =  $_POST['signupemail'];
-        unset($_POST['signupemail']);
-        $signupemail = "";
-    }
-    else if ($db->querySingle("SELECT mail FROM user WHERE mail='$signupemail'") != null){
-        $msg[] = "L'adresse mail saisie existe déjà ! Veuillez nous contacter si vous avez
-            oublier votre identifiant et/ou votre mot de passe.";
-        unset($_POST['signupemail']);
-        $signupemail = "";
-    }
+	$newpage = false;
+	$signupemail = $_POST['signupemail'];
+	// Regexp pour les adresses mail (incomplet, mais suffisant pour la plupart des adresses).
+	// http://en.wikipedia.org/wiki/Email_address
+	// N'implémente pas les "quotes" dans la partie locale (avant le @).
+	$allowed_local = "[-a-zA-Z0-9!#\$%&'*+/^=?_`{|}~]"; /* Je ne sais pas trop si l'espace est autorisée. */
+	$pattern_local = "$allowed_local(\\.?$allowed_local)*";
+	$pattern_hostname_label = '[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])?';
+	$pattern_hostname = "$pattern_hostname_label(\\.$pattern_hostname_label)*";
+	$pattern_ip = "([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
+	$pattern_host = "($pattern_hostname|\\[$pattern_ip\\])";
+	// Note : j'ai utilisé ";" comme délimiteur de regexp car il y a un slash dans $allowed_local, et je ne sais pas comment l'échapper là
+	$pattern = ";^$pattern_local@$pattern_host\$;";
+	
+	if(trim($signupemail) == ""){
+		$msg[] = $strings['err_signup_fill_mail'];
+	}
+	else if (!preg_match($pattern, $signupemail)){
+		// TODO : ce message est erroné.
+		$msg[] = $strings['err_signup_invalid_mail'];
+	}
+	else if ($db->querySingle("SELECT mail FROM user WHERE mail='$signupemail'") != null){
+		$msg[] = $strings['err_signup_existing_mail'];
+	}
 }
 
 if(isset($_POST['signupid'])){
-    $newpage = false;
+	$newpage = false;
 	$signupid = $_POST['signupid'];
-        $pattern = "/^([a-zA-Z0-9])+([\.\-_][a-zA-Z0-9]*)*/";
-    if(trim($signupid) == ""){
-        $msg[] = "Veuillez renseigner le champ 'Choisir un identifiant'.\n";
-        unset($_POST['signupid']);
-        $signupid = "";
-    }
-    else if (!preg_match($pattern, $signupemail)){
-        $msg[] = "Identifiant invalid. Vous pouvez utiliser des lettres, des chiffres et
-            les caractères spéciaux '-', '_' et '.'\n";
-	$signupid =  $_POST['signupid'];
-        unset($_POST['signupid']);
-        $signupid = "";
-    }
-    else if ($db->querySingle("SELECT login FROM user WHERE login='$signupid'") != null){
-        $msg[] = "Identifiant déjà pris ! Veuillez choisir un autre identifiant.";
-        unset($_POST['signupid']);
-        $signupid = "";
-    }
+	$pattern = "/^([a-zA-Z0-9])+([\.\-_][a-zA-Z0-9]*)*/";
+	if(trim($signupid) == ""){
+		$msg[] = $strings['err_signup_fill_login'];
+	}
+	else if (!preg_match($pattern, $signupid)){
+		$msg[] = $strings['err_signup_invalid_login'];
+		$signupid =	 $_POST['signupid'];
+	}
+	else if ($db->querySingle("SELECT login FROM user WHERE login='$signupid'") != null){
+		$msg[] = $strings['err_signup_existing_login'];
+	}
 }
 
 if(isset($_POST['signuppswd1'])){
-    $newpage = false;
-    $signuppswd1 = $_POST['signuppswd1'];
-    if(trim($signuppswd1) == ""){
-        $msg[] = "Veuillez renseigner le champ 'Mot de passe'.\n";
-        unset($_POST['signuppswd1']);
-        $signuppswd1 = "";
-        unset($_POST['signuppswd2']);
-        $signuppswd2 = "";
-    }
-    else  if(strlen($signuppswd1) < 5){
-        $msg[] = "Mot de passe invalide. Votre mot de passe doit comporter au moins 5 caractères.\n";
-        unset($_POST['signuppswd1']);
-        $signuppswd1 = "";
-        unset($_POST['signuppswd2']);
-        $signuppswd2 = "";
-    }
+	$newpage = false;
+	$signuppswd1 = $_POST['signuppswd1'];
+	if(trim($signuppswd1) == ""){
+		$msg[] = $strings['err_signup_fill_passwd1'];
+	}
+	else  if(strlen($signuppswd1) < 5){
+		$msg[] = $strings['err_signup_invalid_passwd1'];
+	}
 }
 
 if(isset($_POST['signuppswd2'])){
-    $newpage = false;
-    $signuppswd2 = $_POST['signuppswd2'];
-    if(trim($signuppswd2) == ""){
-        $msg[] = "Veuillez renseigner le champ 'Resaisir le mot de passe'.\n";
-        unset($_POST['signuppswd1']);
-        $signuppswd1 = "";
-        unset($_POST['signuppswd2']);
-        $signuppswd2 = "";
-    }
-    if(strlen($signuppswd1 != $signuppswd2)){
-        $msg[] = "Les deux mots de passe saisis ne sont pas identiques.\n";
-        unset($_POST['signuppswd1']);
-        $signuppswd1 = "";
-        unset($_POST['signuppswd2']);
-        $signuppswd2 = "";
-    }
+	$newpage = false;
+	$signuppswd2 = $_POST['signuppswd2'];
+	if(trim($signuppswd2) == ""){
+		$msg[] = $strings['err_signup_fill_passwd2'];
+	}
+	if(strlen($signuppswd1 != $signuppswd2)){
+		$msg[] = $strings['err_signup_passwords_dont_match'];
+	}
+}
+
+if(count($msg) == 0 && $newpage == false)
+{
+	$ok = ($db->query("INSERT INTO user(mail, login, hash_passwd, score) VALUES ('" . SQLite3::escapeString($signupemail)
+		. "', '" . SQLite3::escapeString($signupid)
+		. "', '" . SQLite3::escapeString(md5($signuppswd1))
+		. "', 0);"));
+	
+	if($ok == true) {
+		$_SESSION['userId'] = $signupid;
+		return_to($location, "?show_msg=ok_signup_registered");
+	} else {
+		$msg[] = $strings['err_signup_dbinsert'];
+	}
 }
 
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
 		<title>PtiClic sous Android - Version Alpha - Inscription</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" href="ressources/simple.css" />
 	</head>
-
-
-        <body>
-		<div class="menu">
-			<?php include("ressources/menu.inc"); ?>
-		</div>
+	<body>
+		<?php include("ressources/menu.inc"); ?>
 		<div class="content">
-                    <p>Vous n'êtes pas encore inscrit&nbsp;? Inscrivez-vous&nbsp;:</p>
-                    <?php
-			if(sizeof($msg) > 0){
-				echo '<span class="message warning">'.
-                            "<b>Saisie invalide. Les erreurs sont les suivantes : </b> <p>".
-                            "<ul>";
-                            foreach ($msg as $m) {
-                                echo "<li>".$m;
-                            }
-                            echo "</ul>";
-                        } 
-                        else if($newpage == false){
-                            $ok = ($db->query("INSERT INTO user(mail, login, hash_passwd, score) VALUES ('" . SQLite3::escapeString($signupemail)
-                                   . "', '" . SQLite3::escapeString($signupid)
-                                   . "', '" . SQLite3::escapeString(md5($signuppswd1))
-                                   . "', 0);"));
-
-                            if($ok == true)
-                                echo "insertion worked!!!!!";
-                            else
-                                echo "insertion failed!!!";
-                                    //header("location:".$location);
-                           echo '<span class="message success">'."Inscription s'est déroulée avec succès !";
-                           unset($_POST);
-                           $newpage = true;
-                        }
-
-                            echo '</span>';
-		    ?>
-		    <form name="signupform" method="post" action="signup.php?return=<?php echo $location; ?>">
-                        <table class="signuptbl">
-				<tr>
-					<td>
-						<label for="signupemail">Saisir votre adresse mail&nbsp;:</label>
-					</td>
-					<td>
-						<input name="signupemail" type="text"
-                                                       <?php
-                                                       if(isset($_POST['signupemail'])){
-                                                           echo "value='$signupemail'";
-                                                       }
-                                                       ?>
-                                                       /><br />
-					</td>
-				</tr>
-                        	<tr>
-					<td>
-						<label for="signupid">Choisir un identifiant&nbsp;: </label>
-					</td>
-					<td>
-						<input name="signupid" type="text"
-                                                       <?php
-                                                       if(isset($_POST['signupid'])){
-                                                           echo "value='$signupid'";
-                                                       }
-                                                       ?>
-                                                       />
-	                        	</td>
-                                </tr>
-				<tr>
-                                	<td>
-						<label for="signuppswd1">Choisir un mot de passe&nbsp;: </label>
-					</td>
-					<td>
-						<input name="signuppswd1" type="password" />
-	                        	</td>
-				</tr>
-                  		<tr>
-                                	<td>
-						<label for="signuppswd2">Resaisir le mot de passe&nbsp;: </label>
-					</td>
-					<td>
-						<input name="signuppswd2" type="password" />
-	                        	</td>
-				</tr>
-				<tr>
-					<td>
-
-					</td>					
-					<td>
-						<input type="submit" name="signupsubmit" value="Valider" />
-					</td>
-				</tr>
-			</table>
-                    </form>
+			<h2>Inscription</h2>
+			<?php include("ressources/showmsg.inc"); ?>
+			<h3>Vous n'avez pas encore de compte ?</h3>
+			<p>Inscrivez-vous :</p>
+			<?php
+				if(count($msg) > 0)
+				{
+					echo '<div class="message warning">'.
+						 '<p><b>Saisie invalide. Les erreurs sont les suivantes : </b></p>'.
+						 '<ul>';
+					foreach ($msg as $m) {
+						echo "<li>".htmlspecialchars($m)."</li>";
+					}
+					echo '</ul>';
+					echo '</div>';
+				}
+			?>
+			<form name="signupform" method="post" action="signup.php?return=<?php echo $location; ?>">
+				<table class="signuptbl">
+					<tr>
+						<td>
+							<label for="signupemail">Saisir votre adresse mail&nbsp;:</label>
+						</td>
+						<td>
+							<input name="signupemail" type="text"
+								<?php
+									if(isset($_POST['signupemail'])){
+										echo " value='$signupemail'";
+									}
+								?>
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="signupid">Choisir un identifiant&nbsp;: </label>
+						</td>
+						<td>
+							<input name="signupid" type="text"
+								<?php
+									if(isset($_POST['signupid'])){
+										echo " value='$signupid'";
+									}
+								?>
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="signuppswd1">Choisir un mot de passe&nbsp;: </label>
+						</td>
+						<td>
+							<input name="signuppswd1" type="password" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="signuppswd2">Resaisir le mot de passe&nbsp;: </label>
+						</td>
+						<td>
+							<input name="signuppswd2" type="password" />
+						</td>
+					</tr>
+					<tr>
+						<td>
+							
+						</td>					
+						<td>
+							<input type="submit" name="signupsubmit" value="Valider" />
+						</td>
+					</tr>
+				</table>
+			</form>
+			<h3>Vous êtes déjà inscrit ?</h3>
+			<p><a href="login.php?return=<?php echo $location; ?>">Connectez-vous !</a></p>
 		</div>
-
-		<div class="footer">
-			<?php include("ressources/footer.inc"); ?>
-		</div>
+		<?php include("ressources/footer.inc"); ?>
 	</body>
- </html>
+</html>
