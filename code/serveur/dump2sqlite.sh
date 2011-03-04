b274f3ec760a743b5030081708e234ffb413b642
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-03-05 00:02:27 +0100

    Suppression des '\' dans le dump (j'étais certain d'avoir déjà corrigé ce bug…).

--- code/serveur/dump2sqlite.sh
+++ code/serveur/dump2sqlite.sh
@@ -43,7 +43,9 @@ cat "$1" \
 | tr '\r' ' ' \
 | sed -e 's/X/XX/g' | sed -e 's/A/Xa/g' | tr '\n' 'A' | sed -e 's/A")/")/g' | tr 'A' '\n' | sed -e 's/Xa/A/g' | sed -e 's/XX/X/g' \
 | pv -s "$(wc -c "$1" | sed -E -e 's/^ *([0-9]*) .*$/\1/')" \
-| sed -E -e "s#'#''#g" \
+| sed -E \
+  -e "s#\##g" \
+  -e "s#'#''#g" \
   -e 's#^/?// [0-9]+ occurrences of relations ([a-z_]+) \(t=([0-9]+) nom_etendu="([^"]+)" info="([^"]+)"\)$#insert into type_relation(name, num, extended_name, info) values('\''\1'\'', \2, '\''\3'\'', '\''\4'\'');#' \
   -e 's#^/?// [0-9]+ occurrences of nodes ([a-z_]+) \(t=([0-9]+)\)$#insert into type_node(name, num) values('\''\1'\'', \2);#' \
   -e 's#^eid=([0-9]+):n="(.*)":t=([0-9]+):w=(-?[0-9]+)$#insert into node(eid, name, type, weight) values(\1, '\''\2'\'', \3, \4);#' \
