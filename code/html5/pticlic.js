019197e59f48d537f860c745bfbaf4f0c27608aa
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-03-29 15:11:05 +0200

    La version html5 peut itérer sur les mots du nuage.

--- code/html5/pticlic.js
+++ code/html5/pticlic.js
@@ -1,10 +1,32 @@
+if (typeof console == "undefined") { console = {}; }
+if (typeof console.log == "undefined") { console.log = function() {}; }
+
 $(function () {
 	var url = "tmp.json"
 	$.getJSON(url, function(data) {
-		console.log(data);
+		dbg = data;
 		var game = data[0];
 		var currentWordNb = 0;
+		var answers = [];
+		
+		var refresh = function() {
+			$(".currentWord").text(game.cloud[currentWordNb].name);
+			
+			$.each(game.cat, function(rel, cat) {
+				$("#r"+rel).text(cat.name.replace("%mc", game.center.name).replace("%mn", game.cloud[currentWordNb].name));
+			});
+		}
+		
+		refresh();
+		
 		$(".centralWord").text(game.center.name);
-		$(".currentWord").text(game.cloud[currentWordNb].name);
-	})
+		$.each(game.cat, function(rel, cat) {
+			$("#r"+rel)
+				.addClass("rid"+cat.id)
+				.click(function() {
+					answers[currentWordNb++] = cat.id;
+					refresh();
+				});
+		});
+	});
 });
