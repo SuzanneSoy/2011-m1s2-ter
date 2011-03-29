83a3769e1a2137c4470afe1d91d4d2ada2b2a2d8
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-03-29 16:32:11 +0200

    Nettoyage du code, gestion de la fin de partie.

--- code/html5/pticlic.js
+++ code/html5/pticlic.js
@@ -1,32 +1,31 @@
-if (typeof console == "undefined") { console = {}; }
-if (typeof console.log == "undefined") { console.log = function() {}; }
-
 $(function () {
 	var url = "tmp.json"
 	$.getJSON(url, function(data) {
-		dbg = data;
 		var game = data[0];
 		var currentWordNb = 0;
 		var answers = [];
 		
 		var refresh = function() {
-			$(".currentWord").text(game.cloud[currentWordNb].name);
-			
-			$.each(game.cat, function(rel, cat) {
-				$("#r"+rel).text(cat.name.replace("%mc", game.center.name).replace("%mn", game.cloud[currentWordNb].name));
-			});
+			if (currentWordNb < game.cloud.length) {
+				$(".mc").text(game.cloud[currentWordNb].name);
+				$(".mn").text(game.center.name);
+			} else {
+				$(".relations").empty();
+				alert("Partie terminée !");
+			}
 		}
 		
-		refresh();
-		
-		$(".centralWord").text(game.center.name);
-		$.each(game.cat, function(rel, cat) {
-			$("#r"+rel)
+		$.each(game.cat, function(i, cat) {
+			$('<div/>')
+				.html(cat.name.replace(/%(m[cn])/g, '<span class="$1"/>'))
 				.addClass("rid"+cat.id)
 				.click(function() {
 					answers[currentWordNb++] = cat.id;
 					refresh();
-				});
+				})
+				.appendTo(".relations");
 		});
+		
+		refresh();
 	});
 });
