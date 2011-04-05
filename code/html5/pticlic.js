d07d3b03c935e4aa0c54d4b032c5d989af86e513
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-04-05 18:38:58 +0200

    Ajout des boutons de relations dans le .jss .

--- code/html5/pticlic.js
+++ code/html5/pticlic.js
@@ -1,31 +1,78 @@
 function jss() {
 	var w=480, h=800;
 	var mch = h/8, mnh = h*0.075;
+	
+	$("body")
+		.css({
+			padding: 0,
+			margin: 0,
+			textAlign: "left"
+		});
+	
 	$("#screen")
 		.width(w)
 		.height(h)
+		.position({my:"center center", at:"center center", of:"body", collision:"none"});
 	
 	$("#mc-caption-block")
+		.css({
+			position: "absolute"
+		})
 		.width(w)
 		.height(mch)
 		.position({my:"center top", at:"center top", of:"#screen", collision:"none"});
+	
 	$("#mc-caption")
+		.css({
+			maxWidth: w*0.9,
+			textAlign: "center",
+			position: "absolute"
+		})
 		.fitFont(w*0.9, mch*0.9, 20)
-		.css("max-width", w*0.9)
 		.position({my:"center center", at:"center center", of:"#mc-caption-block", collision:"none"});
 	
 	$("#mn-caption-block")
+		.css({
+			borderWidth: h/100,
+			position: "absolute"
+		})
 		.width(w)
 		.height(mnh)
-		.css("border-width", h/100)
 		.position({my:"center top", at:"center bottom", of:"#mc-caption-block", collision:"none"});
+	
 	$("#mn-caption")
+		.css({
+			maxWidth: w*0.9,
+			textAlign: "center",
+			position: "absolute"
+		})
 		.fitFont(w*0.9, mnh*0.9, 20)
-		.css("max-width", w*0.9)
 		.position({my:"center center", at:"center center", of:"#mn-caption-block", collision:"none"});
+	
+	$(".relations > div")
+		.css({
+			margin: 10,
+			height: 72,
+			padding: 10,
+		});
 
-	$("#screen")
-		.position({my:"center center", at:"center center", of:"body", collision:"none"});
+	// TODO : fitFont pour ".relations div"
+	$(".relations > div:nth-child(odd)")
+		.css({
+			backgroundPosition: "2% center", // TODO : virer le pourcentage, et séparer l'icône dans un nouvel élément.
+			textAlign: "right",
+			paddingLeft: 76
+		});
+	
+	$(".relations > div:nth-child(even)")
+		.css({
+			backgroundPosition: "98% center", // TODO : virer le pourcentage, et séparer l'icône dans un nouvel élément.
+			textAlign: "left",
+			paddingRight: 76
+		});
+	
+	$(".relations")
+		.position({my:"center bottom", at:"center bottom", of:"#screen", offset:"0 -10", collision:"none"});
 }
 
 
@@ -53,11 +100,14 @@ $(function () {
 				.addClass("rid"+cat.id)
 				.click(function() {
 					answers[currentWordNb++] = cat.id;
+					$(this).addClass("hot")//.delay(500).removeClass("hot"); // TODO: just blink.
 					refresh();
 				})
 				.appendTo(".relations");
 		});
 		
+		$(window).resize(jss);
 		refresh();
+		refresh(); // TODO : fix the bug with the margin on ".relation > div"
 	});
 });
