04b86bf7ee5aed52a10bf4b354802f95456dc03e
Yoann <yoann.b87@voila.fr>
2011-05-25 22:29:58 +0200

    Correction d'un autre bug dans le script de conversion sql (plus d'erreurs).

--- code/serveur/dump2sqlite.sh
+++ code/serveur/dump2sqlite.sh
@@ -38,8 +38,7 @@ create table random_center_node(eid);
 create table user_info(user, key, value, primary key (user, key));
 
 insert into user(login, mail, hash_passwd, score, ugroup, cgCount) values('$(echo "$user" | sed -e "s/'/''/g")', 'foo@isp.com', '$(echo "$passwd" | 
-dd bs=1 
-count="${#passwd}" | (if which md5sum >/dev/null 2>&1; then md5sum; else md5; fi) | cut -d ' ' -f 1)', 0, 1, 0);
+dd bs=1 count="${#passwd}" | (if which md5sum >/dev/null 2>&1; then md5sum; else md5; fi) | cut -d ' ' -f 1)', 0, 1, 0);
 insert into user(login, mail, hash_passwd, score, ugroup, cgCount) values('bot', 'no mail', 'no password', 0, 1, 0);
 EOF
 
