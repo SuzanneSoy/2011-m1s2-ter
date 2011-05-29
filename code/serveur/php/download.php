d85b88da61a1b4bcadaf6fc6cebbca52c1bf0b46
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2011-05-30 01:10:21 +0200

    Correction du nom du fichier à télécharger.

--- code/serveur/php/download.php
+++ code/serveur/php/download.php
@@ -4,6 +4,8 @@ session_start();
 if(!isset($_SESSION['userId']))
 	header("location:login.php?return=download&showmsg=oth_login_download_nauth");
 
+$dl = "ressources/pticlic-alpha-v0.2.apk"
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
@@ -16,11 +18,11 @@ if(!isset($_SESSION['userId']))
 		<div class="content">
 			<?php include("ressources/showmsg.inc"); ?>
 			<h2>Téléchargement de l'application</h2>			
-			<span class="downloadarea"><a href="ressources/pticlic-alpha-v0.1.apk" id="downloadlink">Téléchargement</a></span>
+			<span class="downloadarea"><a href="<?php echo $dl; ?>" id="downloadlink">Téléchargement</a></span>
 			<h2>Installation de l'application</h2>
 			<h3> A partir de votre téléphone </h3>
 			<ul>
-				<li><a href="ressources/pticlic-alpha-v0.1.apk">Téléchargez le fichier d'installation</a>.</li>
+				<li><a href="<?php echo $dl; ?>">Téléchargez le fichier d'installation</a>.</li>
 				<li>Vous avez besoin d'autoriser votre téléphone à accepter les sources inconnue. Pour cela, allez dans
 					Préférence→Application. Cochez «Sources inconnues».</li>
 				<li>Une fois téléchargé, cliquez sur le fichier dans la barre de notification d'Android™ et suivez les instructions
@@ -30,7 +32,7 @@ if(!isset($_SESSION['userId']))
 			</ul>
 			<h3> A partir de votre ordinateur </h3>
 			<ul>
-				<li><a href="ressources/pticlic-alpha-v0.1.apk">Téléchargez le fichier d'installation</a>.</li>
+				<li><a href="<?php echo $dl; ?>">Téléchargez le fichier d'installation</a>.</li>
 				<li>Transférez ce fichier sur votre téléphone à l'aide de bluetooth, une clé usb ou autre.</li>
 				<li>Depuis votre téléphone, trouvez sur votre carte mémoire l'application que vous venez de transférer.</li>
 				<li>Cliquez sur l'application afin de l'installer sur votre téléphone</li>
